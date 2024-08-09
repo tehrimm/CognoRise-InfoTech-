ee67eecc3bca054a90f9a7cdf0687eb868e57683
Tehrim Maqbool <117945703+tehrimm@users.noreply.github.com>
2024-08-09 15:42:25 +0500

    word.py

---
+++ word.py
@@ -0,0 +1,12 @@
+words_list = [
+    'abandon','tired','brown', 'ability', 'absence', 'academy', 'accept', 'account', 'acquire', 'advice', 'airplane', 'alcohol',
+    'ancient', 'animal', 'apparel', 'attempt', 'balance', 'beauty', 'beyond', 'candle', 'captain', 'circle',
+    'climate', 'commit', 'company', 'concept', 'contact', 'courage', 'crystal', 'culture', 'danger', 'dinner',
+    'disease', 'doctor', 'dollar', 'dynamic', 'eagle', 'eclipse', 'effort', 'elegant', 'energy', 'enjoy',
+    'escape', 'famous', 'fashion', 'feather', 'fortune', 'garden', 'guitar', 'hockey', 'honest', 'horizon',
+    'impact', 'inspire', 'jacket', 'journey', 'jungle', 'knight', 'laptop', 'lemon', 'letter', 'library',
+    'magical', 'market', 'mystery', 'nature', 'neither', 'normal', 'ocean', 'oxygen', 'pencil', 'perfect',
+    'planet', 'power', 'public', 'quiet', 'ranger', 'record', 'respect', 'science', 'season', 'singer',
+    'spirit', 'supply', 'system', 'theory', 'travel', 'unique', 'unveil', 'vacation', 'victory', 'vortex',
+    'warmth', 'wonder', 'yellow', 'youth', 'market', 'zebra', 'zenith', 'abandon', 'balance', 'captain', 'contact'
+]
